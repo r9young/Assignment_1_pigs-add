5273cc47157ca44055ebaa894dd81153ea5f7aaf
r9young <121323016+r9young@users.noreply.github.com>
2023-06-29 19:14:29 +1000

    Add files via upload

---
+++ pigs-add.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+file_add=$1
+
+for file_in_working in *; do # for loop all the files in working directory
+    echo "$file_in_working" | grep -v "^pigs"
+    test "$file_add" = "$file_in_working" &&
+        echo "file is matched"
+        break
+done
+
+
+# step: 1
+# for loop all the files in working directory
+
+#step: 2
+# testing whether the file want to be add to staging area is in working directory
+# if the file has already been add in working directory, 
+# for loop all the file in staging area
+# othwise echo "files does not exit in working directory"
+
+# step 3: 
+# for loop all the file in staging area
+# if the file_add's name is same as teh file name in staging area. then you do diff
+# diff two file - updating the changes as per diff's result 
+# otherwise, adding the file in staging area
