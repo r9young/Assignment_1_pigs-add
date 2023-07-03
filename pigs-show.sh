7e17ebec82238db174ddec05e6f5c791947dffe3
Yang <r9young@hotmail.com>
2023-07-03 13:30:56 +1000

    fixed pigs-show

--- pigs-show.sh
+++ pigs-show.sh
@@ -7,6 +7,7 @@ info="$(echo $input)"
 folder_serial=$(echo $info | cut -d ":" -f1)
 folder_name=$(ls ./.pig/objects/ | grep -E "$folder_serial")
 
+
 file_name=$(echo $info | cut -d ":" -f2)
 
 index_path=".pig/index"
@@ -14,7 +15,7 @@ object_path=".pig/objects"
 
 file_path=".pig/objects/$folder_name/$file_name"
 
-
+#check index
 if [ -z  $folder_serial ]; then # This checks if the variable $folder_serial is empty or not set.
     if [ ! -f "./.pig/index/$file_name" ]; then #This checks if a file does not exist. 
         echo "$0: error: '$file_name' not found in index"
@@ -26,18 +27,24 @@ if [ -z  $folder_serial ]; then # This checks if the variable $folder_serial is 
         done  < "$index_file_path"
     fi
 
-    
+#check objects
 else
-    if [ ! -f "./.pig/objects/$folder_name/$file_name" ]; then 
+    if [ -z "$folder_name" ]; then # the problem of previous code is we forget that $folder_name loop the folder, so we don't need loop again
+        echo "$0: error: unknown commit '$folder_serial'"
+   
+    elif [ ! -f "./.pig/objects/$folder_name/$file_name" ]; then 
         echo "$0: error: '$file_name' not found in commit $folder_serial"
+
     else
         object_file_path="./.pig/objects/$folder_name/$file_name"
         while read -r line 
         do
             echo "$line"
         done  < "$object_file_path"
+        
     fi
 fi
+
 
 
 
