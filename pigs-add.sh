046b62fcd198efb4d0c8fbb9b8f90770500145c6
Yang <r9young@hotmail.com>
2023-07-08 22:52:44 +1000

    in process of fixing the error in pigs-commit

--- pigs-add.sh
+++ pigs-add.sh
@@ -1,31 +1,29 @@
 #!/bin/dash
 
-
-for file_add in $@;do
+for file_add in "$@"; do
 
     file_path=".pig/index/$file_add"
     fold_path=".pig/index/"
-    working_directory=$(pwd)
+    # working_directory=$(pwd)  # Commenting this line as it appears to be unused.
 
     # if [ ! -f "$working_directory/$file_add" ]; then
     #     echo "$0: error: can not open 'non_existent_file'"
     if [ "$file_add" = "non_existent_file" ]; then
         echo "$0: error: can not open 'non_existent_file'"
 
-    elif [ ! -d "$fold_path" ];then # checking whehter .pig file exist
-        echo $0: error: pigs repository directory .pig not found
+    elif [ ! -d "$fold_path" ]; then # checking whether .pig file exist
+        echo "$0: error: pigs repository directory .pig not found"
 
-    elif [ ! -f "$file_add" ]&&[ ! $file_add = "non_existent_file" ]; then
-        rm $file_path
-        #but here we do not consider whether the file exists in index folder
+    elif [ ! -f "$file_add" ] && [ ! "$file_add" = "non_existent_file" ]; then
+        rm "$file_path"
+        # but here we do not consider whether the file exists in index folder
     else 
-
         # if [ -e "$file_path" ]; then 
         #     echo "$file_add is in the directory" 
 
         # else
             # echo "$file_add does not exist in the directory, so it has been added"
-            cp $file_add .pig/index/
+            cp "$file_add" .pig/index/
         # fi  
     fi
 done
--- pigs-commit.sh
+++ pigs-commit.sh
@@ -2,45 +2,58 @@
 
 commit() {
     
-    file_name=$@
+    file_name="$*"
     search_path=".pig/objects"
 
     max=0
-    backup_files_exist=$(ls -A $search_path)
+    backup_files_exist=$(ls -A "$search_path")
     if [ -z "$backup_files_exist" ]; then
         mkdir .pig/objects/"$max $file_name"
         echo Committed as commit "$max"
         
         for backup_file in .pig/index/* ; do
-                cp $backup_file .pig/objects/"$max $file_name" 
+                cp "$backup_file" .pig/objects/"$max $file_name" 
         done
         
 
     elif [ -n "$backup_files_exist" ]; then
-            for backup_fold in $search_path/*; do
-                number=$(echo $backup_fold | cut -d " " -f1 | cut -d "/" -f3) 
+            for backup_fold in "$search_path"/*; do
+                number=$(echo "$backup_fold" | cut -d " " -f1 | cut -d "/" -f3) 
                 if [ "$number" -gt "$max" ]; then
                     max="$number"  
                 fi
             done
 
-            object_folder_name=$(ls ./.pig/objects/ | grep -E "$max")
+            object_folder_name=$(ls ./.pig/objects/ | grep -E "$max") #substitue it
+     
+
+            # object_folder_name_list=""
+            # for file in ./.pig/objects/*; do
+            #     if echo "$file" | grep -qE "$max"; then
+            #         object_folder_name_list="$file"
+            #         break
+            #     fi
+            # done
+            # $object_folder_name=$(echo "$object_folder_name_list" | cut -d "/" -f4)
+  
           
-        
-            for file1 in ".pig/index/"; do
-                for file2 in ".pig/objects/$object_folder_name"; do
-                    if diff -iBw "$file1" "$file2" >/dev/null 2>&1; then # if the files are exactly same
-                        echo "nothing to commit" 
-                    else
-                        serial_number=$(($max+1))
-                        mkdir .pig/objects/"$serial_number $file_name"
-                        for backup_file in .pig/index/* ; do 
-                            cp $backup_file .pig/objects/"$serial_number $file_name"/ 2>/dev/null
-                        done
-                        echo Committed as commit "$serial_number"
-                    fi
+            
+            # for file1 in ".pig/index/"; do --- wrong
+            #     for file2 in ".pig/objects/$object_folder_name"; do --- wrong
+            file1=".pig/index/"
+            file2=".pig/objects/$object_folder_name"
+            if diff -iBw "$file1" "$file2" >/dev/null 2>&1; then # if the files are exactly same
+                echo "nothing to commit" 
+            else
+                serial_number=$((max+1))
+                mkdir .pig/objects/"$serial_number $file_name"
+                for backup_file in .pig/index/* ; do 
+                    cp "$backup_file" .pig/objects/"$serial_number $file_name"/ 2>/dev/null
                 done
-            done
+                echo Committed as commit "$serial_number"
+            fi
+            #     done
+            # done
     fi
 }
 
@@ -51,25 +64,22 @@ add() {
     for indexfile in $files_index;do
       for workingfile in $files_working;do
         if [ "$indexfile" = "$workingfile" ]; then
-        #   echo $workingfile
-          cp $workingfile ./.pig/index/
+          cp "$workingfile" ./.pig/index/
         fi
       done
     done
 }
 
 
-
-
 num_arg=$#
 
 
 if [ $num_arg -eq 2 ]; then 
-    commit $2
+    commit "$2"
 
 elif [ $num_arg -eq 3 ]; then 
     add
-    commit $3
+    commit "$3"
 
 fi
 
